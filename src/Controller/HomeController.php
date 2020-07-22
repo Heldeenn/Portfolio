43c1b55f2fe3b0632217f50b297b826c6808ffcc
heldeenn <amandine.ln7@gmail.com>
2020-07-22 17:04:33 +0200

    ajout section projet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param Request $request
+     * @param ProjectRepository $projectRepository
+     * @return Response
      */
-    public function index()
+    public function index(Request $request, ProjectRepository $projectRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $projects = $projectRepository->findAll();
+        return $this->render('home/index.html.twig', [
+            'projects' => $projects
+        ]);
     }
 }
