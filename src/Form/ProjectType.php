5224fe339363574bf75b90f1443e449dfe15fb20
heldeenn <amandine.ln7@gmail.com>
2020-07-22 17:52:39 +0200

    crud projet

---
+++ src/Form/ProjectType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('dateStart')
+            ->add('dateEnd')
+            ->add('description')
+            ->add('client')
+            ->add('github')
+            ->add('website')
+            ->add('technologies')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
